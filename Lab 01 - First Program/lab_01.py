7ff2da225619915bd1e9c2f1a7ac6fd6dfcf6b65
Daniel Welicki <danielwelicki68@gmail.com>
2021-01-28 12:57:22 -0800

    First Lab, first iteration of lab_01

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print( "Hi! \"私の名前はダニエルです.\" That means my name is Daniel in Japanese!")
+print("I thought that would be cool, because I would have to use escape codes for the quotes.")
+print("I'm a self taught japanese student but I have been slacking a little. I can read Katakana and Hiragana though")
+print('Well, yeah. That\'s enough about me. Used some print statements to write this, badabing, badaboom.')
