281eb7c3ce3f71b7a67a96fe4b8090d2bfeded29
Daniel Welicki <danielwelicki68@gmail.com>
2021-01-29 10:47:42 -0800

    challene project

---
+++ Lab 01 - First Program/cool escape code thingy.py
@@ -0,0 +1,12 @@
+print("I have to use three different escape codes, so here's the first one! (\") I made it so you can see the double"
+      " quote!")
+print("I got to use a second escape code, but"
+      "\n Which"
+      "\n one"
+      "\n should"
+      "\n I"
+      "\n use?"
+      "\n I guess I will use \\n")
+print("I have no idea how to use \\r, so I will just show you a single quote. (\') isn't it magnificent?")
+
+
