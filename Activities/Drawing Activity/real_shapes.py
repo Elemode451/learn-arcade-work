6da1c070faa3374aca1ae05f14451a7158dd101a
Daniel Welicki <danielwelicki68@gmail.com>
2021-02-08 14:11:15 -0800

    real_shapes.py is the assignment, located in the new folder

---
+++ Activities/Drawing Activity/real_shapes.py
@@ -0,0 +1,14 @@
+#Importing Library and creating the window
+import arcade
+arcade.open_window(600, 600, "Very Cool Drawing Created By Me")
+arcade.set_background_color(arcade.csscolor.WHITE)
+arcade.start_render()
+#shapes start here
+arcade.draw_triangle_filled(150, 200, 450, 200, 300, 450, arcade.csscolor.GREEN)
+arcade.draw_circle_filled(300,300,40, arcade.csscolor.WHITE)
+arcade.draw_circle_filled(300,300,20, arcade.csscolor.BLACK)
+arcade.draw_circle_filled(314,315,10, arcade.csscolor.WHITE)
+arcade.draw_text("The Illuminati but for kids", 217.5 , 150, arcade.csscolor.BLACK, font_size=12,)
+#stop rendering here
+arcade.finish_render()
+arcade.run()
